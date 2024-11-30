9c0c9880e05f6b157dcb7511d0f806b7f9edc1de
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2024-11-29 19:49:57 -0500

    ?

--- setup/ubuntu.sh
+++ setup/ubuntu.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-sudo apt update
-sudo apt install gcc pkg-config python3-venv libssl-dev pipx iverilog
+sudo apt-get update
+sudo apt-get install gcc pkg-config python3-venv libssl-dev pipx iverilog
 pipx install maturin==1.2.3
 pipx ensurepath
 sudo snap install zig --classic --beta
-cargo install --git git@gitlab.com:spade-lang/swim.git
