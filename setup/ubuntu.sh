0769f36a95d0d134a7df7d578a1d66d3d19260b2
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2024-11-29 20:37:31 -0500

    rookie mistake

--- setup/ubuntu.sh
+++ setup/ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get install gcc pkg-config python3-venv libssl-dev pipx iverilog
+sudo apt-get install gcc pkg-config python3-venv libssl-dev pipx iverilog verilator
 pipx install maturin==1.2.3
 pipx ensurepath
 sudo snap install zig --classic --beta
